52657400be11711f6e3307cf786d4d99c7b7c225
Ales Michel <ales.michel@centrum.cz>
2025-03-14 18:01:51 +0100

    Aded textarea, position component

--- cms/src/Components/TextArea.php
+++ cms/src/Components/TextArea.php
@@ -1,1 +1,38 @@
 <?php
+
+include_once("TextField.php");
+
+class TextArea extends TextField
+{
+    public function getDataFieldsForEdit(): string
+    {
+        $out = '';
+        if ($this->componentIsMultlang === 1) {
+            $out .= "<label for='textArea_" . $this->componentName . "' class='form-label mt-2 mb-1'>" . $this->componentName . " CZ</label>";
+            $out .= "<textarea class='form-control' id='textArea_" . $this->componentName . "' name='component_" . $this->componentName . "' placeholder='...' " . ($this->componentIsRequired ? 'required' : '') . ">" . htmlspecialchars($this->componentData) . "</textarea>";
+
+            $out .= "<label for='textArea_" . $this->componentName . "_en' class='form-label mt-2 mb-1'>" . $this->componentName . " EN</label>";
+            $out .= "<textarea class='form-control' id='textArea_" . $this->componentName . "_en' name='component_en_" . $this->componentName . "' placeholder='...' " . ($this->componentIsRequired ? 'required' : '') . ">" . htmlspecialchars($this->componentDataEn) . "</textarea>";
+        } else {
+            $out .= "<label for='textArea_" . $this->componentName . "' class='form-label mt-2 mb-1'>" . $this->componentName . "</label>";
+            $out .= "<textarea class='form-control' id='textArea_" . $this->componentName . "' name='component_" . $this->componentName . "' placeholder='...' " . ($this->componentIsRequired ? 'required' : '') . ">" . htmlspecialchars($this->componentData) . "</textarea>";
+        }
+        return $out;
+    }
+
+    public function getDataFieldsForInsert(): string
+    {
+        $out = '';
+        if ($this->componentIsMultlang === 1) {
+            $out .= "<label for='textArea_" . $this->componentName . "' class='form-label mt-2 mb-1'>" . $this->componentName . " CZ</label>";
+            $out .= "<textarea class='form-control' id='textArea_" . $this->componentName . "' name='component_" . $this->componentName . "' placeholder='...' " . ($this->componentIsRequired ? 'required' : '') . "></textarea>";
+
+            $out .= "<label for='textArea_" . $this->componentName . "_en' class='form-label mt-2 mb-1'>" . $this->componentName . " EN</label>";
+            $out .= "<textarea class='form-control' id='textArea_" . $this->componentName . "_en' name='component_en_" . $this->componentName . "' placeholder='...' " . ($this->componentIsRequired ? 'required' : '') . "></textarea>";
+        } else {
+            $out .= "<label for='textArea_" . $this->componentName . "' class='form-label mt-2 mb-1'>" . $this->componentName . "</label>";
+            $out .= "<textarea class='form-control' id='textArea_" . $this->componentName . "' name='component_" . $this->componentName . "' placeholder='...' " . ($this->componentIsRequired ? 'required' : '') . "></textarea>";
+        }
+        return $out;
+    }
+}
