43a46208c4e30a6416c054d78e572ee751fdc5cf
Ales Michel <ales.michel@centrum.cz>
2025-03-18 14:02:47 +0100

    new date component

--- cms/admin/modules/module/components/create.php
+++ cms/admin/modules/module/components/create.php
@@ -11,11 +11,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 //    $currentModule = $_GET['current_module']; // Get current module
 //    $componentId = $_POST['component_id'];  // Get the selected component's ID
     //now we have components id and we wil fetch its name by id
-    
-    $out = '';
 
-    $out .= componentCommon::renderComponents($db);
 
+
+    $out = componentCommon::renderComponents($db);
     $buildPage = new cmsDefaultPage($out);
     $buildPage->buildLayout();
 
---
+++ cms/src/Components/Date.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once("TextField.php");
+
+class Position extends TextField
+{
+    public function getDataFieldsForEdit(): string
+    {
+        $out = '';
+        if ($this->componentIsMultlang === 1) {
+            $out .= "<label for='position_" . $this->componentName . "' class='form-label mt-2 mb-1'>" . $this->componentName . " CZ</label>";
+            $out .= "<input class='form-control' type='date' id='position_" . $this->componentName . "' value='" . htmlspecialchars($this->componentData) . "' name='component_" . $this->componentName . "' " . ($this->componentIsRequired ? 'required' : '') . " />";
+
+            $out .= "<label for='position_" . $this->componentName . "_en' class='form-label mt-2 mb-1'>" . $this->componentName . " EN</label>";
+            $out .= "<input class='form-control' type='date' id='position_" . $this->componentName . "_en' value='" . htmlspecialchars($this->componentDataEn) . "' name='component_en_" . $this->componentName . "' " . ($this->componentIsRequired ? 'required' : '') . " />";
+        } else {
+            $out = "<label for='position_" . $this->componentName . "' class='form-label mt-2 mb-1'>" . $this->componentName . "</label>";
+            $out .= "<input class='form-control' type='date' id='position_" . $this->componentName . "' value='" . htmlspecialchars($this->componentData) . "' name='component_" . $this->componentName . "' " . ($this->componentIsRequired ? 'required' : '') . " />";
+        }
+        return $out;
+    }
+
+    public function getDataFieldsForInsert(): string
+    {
+        $out = '';
+        if ($this->componentIsMultlang === 1) {
+            $out .= "<label for='position_" . $this->componentName . "' class='form-label mt-2 mb-1'>" . $this->componentName . " CZ</label>";
+            $out .= "<input class='form-control' type='date' id='position_" . $this->componentName . "' value='' name='component_" . $this->componentName . "' " . ($this->componentIsRequired ? 'required' : '') . " />";
+
+            $out .= "<label for='position_" . $this->componentName . "_en' class='form-label mt-2 mb-1'>" . $this->componentName . " EN</label>";
+            $out .= "<input class='form-control' type='date' id='position_" . $this->componentName . "_en' value='' name='component_en_" . $this->componentName . "' " . ($this->componentIsRequired ? 'required' : '') . " />";
+        } else {
+            $out .= "<label for='position_" . $this->componentName . "' class='form-label mt-2 mb-1'>" . $this->componentName . "</label>";
+            $out .= "<input class='form-control' type='date' id='position_" . $this->componentName . "' value='' name='component_" . $this->componentName . "' " . ($this->componentIsRequired ? 'required' : '') . " />";
+        }
+        return $out;
+    }
+}
